2a7907902385f679161a3c36c1cfd6ca71521a4a
comunello <comunello@gmail.com>
2017-11-12 00:05:51 -0300

    Update d3LineGraf.js

--- TPfinal/scripts/d3LineGraf.js
+++ TPfinal/scripts/d3LineGraf.js
@@ -1,17 +1,19 @@
 function _chamada(teste) {
-    $(document).on('mouseover mouseout', '.boxGraf', function(){
-     var $div = $("<div>", {id: "viz"}); 
-    $(".boxGraf").append($div);
-     	if (document.contains(document.getElementById("viz"))) {
-            $(document).off('mouseover mouseout');
-            var text = $('.containerID').text();
-            grafica(text);
+	$(document).on('mouseover mouseout', '.boxGraf', function(){
+		var $div = $("<div>", {id: "viz"}); 
+		$(".boxGraf").append($div);
+		if (document.contains(document.getElementById("viz"))) {
+			$(document).off('mouseover mouseout');
+			var text = $('.containerID').text();
+			$(".mensageToGraf").addClass("divoff");
+			grafica(text);
 		}else{
-			  alert("Problemas con a criacao da DIV!!");
-             $(document).off('mouseover mouseout');
+			alert("Problemas con a criacao da DIV!!");
+		$(document).off('mouseover mouseout');
 		}
 	});
 }
+
 var url = 'https://comunello.github.io/infoVis/dadosTabela.tsv';
 function grafica(_id) {
 	d3.tsv(url,
