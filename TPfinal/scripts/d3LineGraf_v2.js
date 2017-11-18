907695b49999130f440997dc208f6e131884e889
comunello <comunello@gmail.com>
2017-11-18 12:33:13 -0300

    Update d3LineGraf_v2.js

--- TPfinal/scripts/d3LineGraf_v2.js
+++ TPfinal/scripts/d3LineGraf_v2.js
@@ -23,22 +23,14 @@ function _chamada() {
     $(document).on('mouseover mouseout', '.boxGraf', function(){
 		var $div = $("<div>", {id: "viz"}); 
 		$(".boxGraf").append($div);
+
 		
-		
-		//$(".infoDir").children( ".botao:eq(0)").attr({id: "botao0"});
-		//$(".infoDir").children( ".botao:eq(1)").attr({id: "botao1"});
-		//$(".infoDir").children( ".botao:eq(2)").attr({id: "botao2"});
-		
-		 
+		var codes;
 		
      	if (document.contains(document.getElementById("viz"))) {
             $(document).off('mouseover mouseout');
-			
            
-			var retornoGraf = grafica3($('.containerID').text(),"total");
-			
-			alert("Teste3: "+ retornoGraf.compra);
-			
+			 retornoGraf = grafica3($('.containerID').text(),"total");
 		}else{
 			  alert("Problemas con a criacao da DIV!!");
              $(document).off('mouseover mouseout');
@@ -55,26 +47,25 @@ $(".infoDir").children( ".botao").click(function() {
 });
 
 function _chamadaBotao(_recebo) {
-	
-	
-
 	if (document.contains(document.getElementById("viz"))) {
-
-		$("#boxEsq p span").empty();
-	  //grafica($('.containerID').text(),text,_recebo);
-
-	}else{
-		 alert("Problemas con a DIV grafica!!");
-	}
+		$(".boxEsq p span").empty();
+		grafica3($('.containerID').text(),_recebo);
+	}else
+		alert("Problemas con a DIV grafica!!");
 };
 
-
-
+function _fdp(code){
+	$(".textCont:eq(0)").children( ".visualText:eq(0)").children( "span").text(code.styl);
+	$(".textCont:eq(0)").children( ".visualText:eq(1)").children( "span").text(code.compra);
+	$(".textCont:eq(1)").children( ".visualText:eq(0)").children( "span").text(code.styl);
+	$(".textCont:eq(1)").children( ".visualText:eq(1)").children( "span").text(code.venda);
+	$(".textCont:eq(2)").children( ".visualText:eq(0)").children( "span").text(code.styl);
+	$(".textCont:eq(2)").children( ".visualText:eq(1)").children( "span").text(code.posVenda);
+}
 
 function grafica3(_id,_type) {
 	var url = 'https://comunello.github.io/infoVis/TPfinal/dadosTabela.tsv';
-	var codes;
-	var filterId2 ;
+
 	d3.tsv(url,
         function(error, data) {
             callbackError = error;
@@ -85,8 +76,6 @@ function grafica3(_id,_type) {
 			d.date= +d.date; //identificar como colocar a data aqui
             d.Descrip=d.Descrip;
 			});
-
-      
 		function _subst(check1,check2) {
 			var _n = check2.length;
 			var i = check1.substr(0,_n);
@@ -96,40 +85,26 @@ function grafica3(_id,_type) {
 			return false;
 		}
   
-   
 		filterId2 = data.filter(function(d) { return d.id ==_id && 
                  _subst(d.Descrip,_type); });
-
-		//console.log(filterId2);
-		var _Compra 	=  d3.sum(filterId2.filter(function(d) { return d.Descrip ==_type.concat("Compra");}), 
+		_Compra 	=  d3.sum(filterId2.filter(function(d) { return d.Descrip ==_type.concat("Compra");}), 
 							function(d) {return d.Value; });
-		var _Venda 		=  d3.sum(filterId2.filter(function(d) { return d.Descrip ==_type.concat("Venda");}), 
+		_Venda 		=  d3.sum(filterId2.filter(function(d) { return d.Descrip ==_type.concat("Venda");}), 
 							function(d) {return d.Value; });
-		var _PosVenda 	=  d3.sum(filterId2.filter(function(d) { return d.Descrip ==_type.concat("PosVenda");}), 
+		_PosVenda 	=  d3.sum(filterId2.filter(function(d) { return d.Descrip ==_type.concat("PosVenda");}), 
 							function(d) {return d.Value; });
 
-		//retorno.type = _type;
-		//retorno.compra = _Compra;
-		//retorno.venda = _Venda;
-		//retorno.posVenda = _PosVenda;
+		var codes = retorno(_type,_Compra,_Venda,_PosVenda);
 		
+		_fdp(codes);
 		
-		codes = retorno(_type,_Compra,_Venda,_PosVenda);
-		alert("TESTE1:"+codes.venda);
-		
-       //   var visualization = d3plus.viz()
-		//						.container("#viz")  // container DIV to hold the visualization
-			//					.data(filterId2)  // data to use with the visualization
-				//				.type("line")       // visualization type
-                  //              .id(["Descrip"])
-					//			.y("Value")         // key to use for y-axis
-						//		.x("date")          // key to use for x-axis
-							//	.draw();  
-     
-		
-   
+		var visualization = d3plus.viz()
+							.container("#viz")  // container DIV to hold the visualization
+							.data(filterId2)  // data to use with the visualization
+							.type("line")       // visualization type
+							.id(["Descrip"])
+							.y("Value")         // key to use for y-axis
+							.x("date")          // key to use for x-axis
+							.draw();  
 	});	
-	alert("TESTE2:"+codes.venda);
-	return codes;
 }
-//grafica3(1,"total");
